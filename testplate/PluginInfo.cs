b03adcb885831dd555d29cb748da1d037a6c201e
TOGGLE <thatsizaicbaby1150@gmail.com>
2026-05-03 15:21:59 -0600

    stupid

--- testplate/PluginInfo.cs
+++ testplate/PluginInfo.cs
@@ -3,6 +3,6 @@
         public const string GUID = "org.pokruk.gorillatag.cameramod";
         public const string Name = "PokRuk's Camera Mod; MelonLoader Edition forked by Toggle";
         public const string Author = "PokRuk, Toggle (@mindwastoggled, @togglegtag)";
-        public const string Version = "3.8.1";
+        public const string Version = "1.0.0";
     }
 }
